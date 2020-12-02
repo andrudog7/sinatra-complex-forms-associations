bbb5e1e4d8ba923497fdd19d854c5bae218f65b7
--add <andrewksmoker@gmail.com>
2020-12-02 15:45:30 -0500

    commit tree 8d49e64d7bcc0e6efb434f2dea481c06ea9b878d

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,5 @@
 sophie = Owner.create(name: "Sophie")
 Pet.create(name: "Maddy", owner: sophie)
 Pet.create(name: "Nona", owner: sophie)
+andrew = Owner.create(name: "Andy")
+Pet.create(name: "Winston", owner: andrew)
